0c466ec00b325a8a9d0052e676060c646948803f
midCenturyDude7 <mgriffes@gmail.com>
2024-03-26 20:39:58 -0700

    Configured plop to autogenerate folders/files

---
+++ app/components/App.tsx
@@ -0,0 +1,8 @@
+// App.tsx Component
+
+export default function App() {
+  return (
+    <div>
+      <h1>App</h1>
+    </div>)
+}
---
+++ app/lib/framer.tsx
@@ -0,0 +1,11 @@
+// framer helper library
+
+import React from 'react';
+
+export default function framer() {
+  return (
+    <div>
+      <h1>framer</h1>
+    </div>
+  );
+}
